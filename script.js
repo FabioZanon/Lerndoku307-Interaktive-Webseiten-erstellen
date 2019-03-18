5fb9410bc2dd98a6312139350c43068db75e0c89
FabioZanon <fabio.zanon@bbz-sh.ch>
2019-03-18 10:45:19 +0100

    Zwischenstand

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var Beispiel
+Beispiel = 4;
+
+var Beispiel2
+Beispiel2 = 5;
+
+var Total
+Total = Beispiel + Beispiel2;               
+
+
+var Beispiel
+Beispiel = "Das Hier";
+
+var Beispiel
+Beispiel = 4;                       
