2d3fe26cc9630a93fe0b59bf60a0e31509b48b60
FabioZanon <fabio.zanon@bbz-sh.ch>
2019-03-18 13:44:50 +0100

    zwischenstand

--- script.js
+++ script.js
@@ -6,12 +6,4 @@ Beispiel = 4;
 var Beispiel2
 Beispiel2 = 5;
 
-var Total
-Total = Beispiel + Beispiel2;               
-
-
-var Beispiel
-Beispiel = "Das Hier";
-
-var Beispiel
-Beispiel = 4;                       
+text.write(Beispiel + Beispiel2);
